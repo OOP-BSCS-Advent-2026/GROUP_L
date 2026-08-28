36a82c6e5c2a0d7ee9fe63d6edbcf8d93018bc3c
Ntege Keith Rowland <rowlandkei70@gmail.com>
2026-08-28 16:29:43 +0300

    Made a few adjustments

--- GROUPL_BUSINESSIMULATOR.java
+++ GROUPL_BUSINESSIMULATOR.java
@@ -113,7 +113,7 @@ public class GROUPL_BUSINESSIMULATOR {
             System.out.printf(Locale.US, "%d. %-20s Qty: %-4d Subtotal: UGX %,10.2f  (%s)%n",
                     i + 1, itemNames[i], quantities[i], subtotals[i], discountLabel);
         }
-        System.out.println("------------------------------------------------------------");
+        System.out.println("============================================================");
         System.out.printf(Locale.US, "GRAND TOTAL: UGX %,.2f%n", grandTotal);
         System.out.println("============================================================");
     }
