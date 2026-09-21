1adde6f47103244907c5229b3d49aa81f8c99870
AINOMUGISHA EMMANUEL JONATHAN <ainomugishaemma@gmail.com>
2026-09-21 15:14:23 +0300

    Add NoDiscountItem class extending Item

---
+++ my folder/NoDiscountItem.java
@@ -0,0 +1,9 @@
+//    No override needed: Item's calculateTotal() is already correct.
+
+class NoDiscountItem extends Item {
+
+    public NoDiscountItem(String name, double price) {
+        super(name, price);
+    }
+}
+
