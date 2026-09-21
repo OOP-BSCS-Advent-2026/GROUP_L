6f7d196246e8779540911a8835f9fe68be05911d
among sherinah <321797476+amongsherinah-netizen@users.noreply.github.com>
2026-09-21 11:43:36 +0300

    Create PercentDiscountItem.java
    Added PercentDiscountItem file

---
+++ my folder/PercentDiscountItem.java
@@ -0,0 +1,25 @@
+/**
+ * GROUP L - QuickPrint Shop
+ * The two items chosen are Photocopy and Lamination.
+ */
+public class PercentDiscountItem extends Item {
+
+    private int threshold;
+    private double percentOff;
+
+    public PercentDiscountItem(String name, double price,
+                               int threshold, double percentOff) {
+        super(name, price);
+        this.threshold = threshold;
+        this.percentOff = percentOff;
+    }
+
+    @Override
+    public double calculateTotal(int quantity) {
+        double subtotal = super.calculateTotal(quantity);
+        if (quantity >= threshold) {
+            subtotal = subtotal * (1 - percentOff / 100.0);
+        }
+        return subtotal;
+    }
+}
